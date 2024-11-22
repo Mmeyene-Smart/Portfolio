d3be04ede77b8a9f00f0c7afeb93a07e8fd7df51
Mmeyene <mmeyenealoysius@gmail.com>
2024-11-22 12:30:19 +0100

    my portfolio

---
+++ src/Components/HeroSection.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+const HeroSection = () => {
+  return (
+    <section class="hero-section" id="hero-section">
+            <div class="container">
+                
+                    <div class="hero-data">
+                        <h1 class="h1">
+                            Hi,<br/> I'm <b>Mmeyene Aloysius</b>.<br/>Frontend Developer
+                        </h1>
+                        <p class="h3">I love building (and occasionally designing) exceptional digital experiences with modern technologies.</p>
+                        <div class="btn-group">
+                            <a href="#contact-me" class="btn btn-primary">Contact Me</a>
+                            <a href="#portfolio-section" class="btn btn-secondary">See My Projects</a>
+                        </div>                        
+                    </div>
+                    <div class="hero-img-wrapper">
+                        <div class="img-box">
+                            <img class="hero-img" src="./Images/Mmeyene.jpg" alt="this is me Mmeyene's Photo"/>
+                        </div>
+                        <div class="shape shape-1"></div>
+                        <div class="shape shape-2"></div>
+                    </div>       
+            </div>           
+        </section>
+  )
+}
+
+export default HeroSection;
